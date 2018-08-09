54f43c930c403c421ec25a07e0548b4e5ffcc570
MagnumBong <tommybowles33@gmail.com>
2018-08-09 02:11:54 +0100

    Got a basic database system functioning

--- main.js
+++ main.js
@@ -3,10 +3,19 @@ const discord = require('discord.js')
 const mainClass = require('./mainClass.js')
 const bot = mainClass.bot
 
+// plugins
+const order = require('./order.js')
+
 // other requirements
 const fs = require('fs')
 
 // main
 bot.on('ready', ()=>{
 	bot.user.setGame("MC Mental.")
+})
+
+bot.on('message', (x)=>{
+	if(x.content.startsWith('/')){
+		order.order(x, x.content.substr(1))
+	}
 })
